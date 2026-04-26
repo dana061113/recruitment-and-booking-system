0798d8de92d9daf06a919c1328bcc453d8c2872d
dana061113 <danakobylandykyzy@gmail.com>
2026-04-26 17:55:21 +0500

    Add VenueAdmin class for event management

---
+++ src/booking/VenueAdmin.java
@@ -0,0 +1,12 @@
+package booking;
+
+public class VenueAdmin {
+
+    public void prepareEvent() {
+        System.out.println("Подготовка мероприятия");
+    }
+
+    public void receiveReport() {
+        System.out.println("Отчет получен");
+    }
+}
